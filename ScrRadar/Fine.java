31e465ba7a381d79d9096507d16c5d1c5755068d
Hosam Zakaria <hossamzakaria61@gmail.com>
2026-07-24 05:54:55 +0300

    Refactor Fine class by removing printFine method

--- ScrRadar/Fine.java
+++ ScrRadar/Fine.java
@@ -10,9 +10,6 @@ public class Fine {
     public void addViolation(Violation vv){
         v.add(vv);
     }
-    public String getString(){
-        return n;
-    }
     public String getPlateNumber(){
         return n;
     }
@@ -24,12 +21,5 @@ public class Fine {
         for(Violation vv : v){ sum += vv.getMoney(); }
         return sum;
     }
-    public void printFine(){
-        System.out.println("Traffic fine for car " + n);
-        System.out.println("Total amount: " + getTotalAmount() + " EGP");
-        System.out.println("Violations:");
-        for(Violation vv : v){
-            System.out.println("- " + vv.getText() + " : " + vv.getMoney() + " EGP");
-        }
-    }
+    
 }
