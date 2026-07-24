c65ec36fa29ba58eac66fb036cf432153e2f8813
Hosam Zakaria <hossamzakaria61@gmail.com>
2026-07-24 05:57:50 +0300

    Refactor SpeedRule to use HashMap for speed limits

--- ScrRadar/SpeedRule.java
+++ ScrRadar/SpeedRule.java
@@ -1,13 +1,19 @@
 package ScrRadar;
-
+import java.util.HashMap;
 public class SpeedRule implements Rule{
+    private HashMap<String, Integer> limit;
+    public SpeedRule(){
+        limit = new HashMap<>();
+        limit.put("PRIVATE", 80);
+        limit.put("TRUCK", 60);
+        limit.put("BUS", 100); 
+    }
     public Violation check(Observation ob){
-        int mx = 0;
-        if(ob.getCarType().equalsIgnoreCase("PRIVATE")) { mx = 80; }
-        else if(ob.getCarType().equalsIgnoreCase("TRUCK")) { mx = 60; }
-        else{
-            return null; 
-        }
+        String type = ob.getCarType().toUpperCase();
+
+        if(!limit.containsKey(type)){ return null; }
+
+        int mx = limit.get(type);
         if(ob.getSpeed() > mx){
             return new Violation("speed of " + ob.getSpeed() + " exceeded max allowed " + mx, 300);
         }
